cbb99a91bd6b66f18f8aa4b41947d406f6157330
Julian Herzfeld <herzfeldjulian@hotmail.de>
2025-10-29 06:00:36 +0100

    graphics added, game.js init() function added, Character class basic structure added, MovableObjekt class basic structure added, pufferfish basic structure added, World class basic structure and draw() function added, chicken class renamed to pufferfish, water class created

---
+++ models/character.class.js
@@ -0,0 +1,13 @@
+class Character extends MovableObjekt {
+
+
+
+
+    constructor() {
+        super().loadImage('img/1.Sharkie/3.Swim/1.png');
+    }
+
+    jump() {
+
+    }
+}
---
+++ models/movable-object.class.js
@@ -0,0 +1,25 @@
+class MovableObjekt {
+    x = 120;
+    y = 300;
+    img;
+    width = 150;
+    height = 120;
+
+
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+
+    moveRight() {
+
+    }
+
+
+    moveLeft() {
+
+    }
+}
